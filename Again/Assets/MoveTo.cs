50d89b06c985dcd6fe1167894911604d43e5c98c
heatondev <heatondev@gmail.com>
2019-04-17 11:21:35 -0600

    Testing AI NavMesh
    Probably won't do this

---
+++ Again/Assets/MoveTo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour {
+
+    public Transform goal;
+    public float[] locations;
+
+    void Start()
+    {
+       
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+        UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.destination = goal.position;
+	}
+}
